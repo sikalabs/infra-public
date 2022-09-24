2a3f6fe71d9e6c6d499e0a2c1bb74181b98f1640
Ondrej Sika <ondrej@ondrejsika.com>
2022-09-24 19:55:12 +0200

    feat(env/do_dev): Add config for CRC VM (crc.dev)

--- env/do_dev/vms_config.tf
+++ env/do_dev/vms_config.tf
@@ -5,6 +5,11 @@ locals {
       record_name  = "example"
       size         = "s-1vcpu-2gb"
     })
+    crc = merge(local.default_do_vm, {
+      droplet_name = "crc"
+      record_name  = "crc"
+      size         = "s-4vcpu-8gb"
+    })
   }
   vms = {
   }
