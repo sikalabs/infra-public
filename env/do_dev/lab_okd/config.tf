ced70c9cf0060280921aa78e1f6d2a9f5c19a984
Ondrej Sika <ondrej@ondrejsika.com>
2022-09-26 09:15:52 +0200

    feat(env/do_dev/lab_okd): Create lab{0..3} for OKD training

--- env/do_dev/lab_okd/config.tf
+++ env/do_dev/lab_okd/config.tf
@@ -1,5 +1,8 @@
 locals {
   vms = {
     "0" = merge(local.vm_templates.lab, {})
+    "1" = merge(local.vm_templates.lab, {})
+    "2" = merge(local.vm_templates.lab, {})
+    "3" = merge(local.vm_templates.lab, {})
   }
 }
