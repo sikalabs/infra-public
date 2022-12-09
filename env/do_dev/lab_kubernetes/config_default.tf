2ad370cb449cd4dd4ba922fdc76fd76e4e0535b8
Ondrej Sika <ondrej@ondrejsika.com>
2022-12-09 09:04:51 +0100

    feat(lab_kubernetes): Add code-server

--- env/do_dev/lab_kubernetes/config_default.tf
+++ env/do_dev/lab_kubernetes/config_default.tf
@@ -12,6 +12,13 @@ write_files:
   content: |
     #!/bin/sh
     kubectl $@
+- path: /root/.config/code-server/config.yaml
+  permissions: "0755"
+  owner: root:root
+  content: |
+    auth: password
+    password: asdfasdf2020
+    cert: false
 runcmd:
   - |
     rm -rf /etc/update-motd.d/99-one-click
@@ -22,6 +29,9 @@ runcmd:
     curl -fsSL https://raw.githubusercontent.com/sikalabs/slu/master/install.sh | sudo sh
     slu install-bin training-cli -v v0.5.0-dev-7
     HOME=/root training-cli kubernetes vm-setup
+    curl -fsSL https://code-server.dev/install.sh | HOME=/root sh
+    systemctl enable --now code-server@root
+    docker run -d --name proxy-8080 --net host sikalabs/slu:v0.50.0 slu proxy tcp -l :80 -r 127.0.0.1:8080
 EOF
   vm_templates = {
     lab = merge(var.default_do_vm, {
