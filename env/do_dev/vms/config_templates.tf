ebaf0f7ad8d5157a729743b10561e08bdbfe397b
Ondrej Sika <ondrej@ondrejsika.com>
2023-01-30 09:13:12 +0100

    feat(env/do_dev/vms): Add rm -rf /root/snap to lab VM cloud init

--- env/do_dev/vms/config_templates.tf
+++ env/do_dev/vms/config_templates.tf
@@ -178,5 +178,7 @@ runcmd:
     sudo apt-get install -y apt-transport-https
     echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
     sudo apt-get update
+
+    rm -rf /root/snap
 EOF
 }
