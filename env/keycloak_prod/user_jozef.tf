73cdbe76b25b6d2df4bbfed6f4b4ab68390fe7fe
Ondrej Sika <ondrej@ondrejsika.com>
2023-04-09 14:25:34 +0200

    feat(env/keycloak_prod): Add Josef Vojtek to SSO

---
+++ env/keycloak_prod/user_jozef.tf
@@ -0,0 +1,21 @@
+resource "keycloak_user" "jozefvojtek" {
+  realm_id       = keycloak_realm.sikalabs.id
+  username       = "jozefvojtek"
+  enabled        = true
+  email          = "jozef.vojtek@sikalabs.com"
+  email_verified = true
+  first_name     = "Jozef"
+  last_name      = "Vojtek"
+  initial_password {
+    value     = var.user_initial_password
+    temporary = true
+  }
+}
+
+resource "keycloak_user_groups" "jozefvojtek" {
+  realm_id = keycloak_realm.sikalabs.id
+  user_id  = keycloak_user.jozefvojtek.id
+  group_ids = [
+    keycloak_group.argocd-admin.id,
+  ]
+}
