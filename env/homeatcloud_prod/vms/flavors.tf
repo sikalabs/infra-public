ba0aacd98a20f06e10baa0f29fd9ede728995675
Ondrej Sika <ondrej@ondrejsika.com>
2023-04-09 15:37:41 +0200

    feat(env/homeatcloud_prod): Add flavor 2CPU 2RAM

--- env/homeatcloud_prod/vms/flavors.tf
+++ env/homeatcloud_prod/vms/flavors.tf
@@ -2,6 +2,7 @@ locals {
   FLAVORS = {
     FLAVOR_8CPU_8RAM = "14" // b1.xlarge
     FLAVOR_4CPU_4RAM = "13" // b1.large
+    FLAVOR_2CPU_2RAM = "12" // b1.medium
     FLAVOR_1CPU_1RAM = "11" // b1.small
   }
 }
