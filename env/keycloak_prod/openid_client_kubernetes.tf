0f5e24632c541969054984da46a8255522fadb14
Ondrej Sika <ondrej@ondrejsika.com>
2023-08-31 23:41:46 +0200

    format(env/keycloak_prod/openid_client_kubernetes): Reformat using terraform fmt

--- env/keycloak_prod/openid_client_kubernetes.tf
+++ env/keycloak_prod/openid_client_kubernetes.tf
@@ -11,10 +11,10 @@ resource "keycloak_openid_client" "kubernetes_rke2_single_node" {
 }
 
 resource "keycloak_openid_audience_protocol_mapper" "kubernetes_rke2_single_node" {
-  realm_id  = keycloak_realm.sikalabs.id
-  client_id = keycloak_openid_client.kubernetes_rke2_single_node.id
-  name      = "audience-mapper"
-included_client_audience = keycloak_openid_client.kubernetes_rke2_single_node.client_id
+  realm_id                 = keycloak_realm.sikalabs.id
+  client_id                = keycloak_openid_client.kubernetes_rke2_single_node.id
+  name                     = "audience-mapper"
+  included_client_audience = keycloak_openid_client.kubernetes_rke2_single_node.client_id
 }
 
 resource "keycloak_openid_client_default_scopes" "kubernetes_rke2_single_node" {
