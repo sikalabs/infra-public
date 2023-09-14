763636ca41b4ff76b5897e80c9ed054db44715c8
Ondrej Sika <ondrej@ondrejsika.com>
2023-09-14 08:01:11 +0200

    feat(keycloak_sikademo): Add groups for ArgoCD

--- env/keycloak_sikademo/groups.tf
+++ env/keycloak_sikademo/groups.tf
@@ -7,3 +7,13 @@ resource "keycloak_group" "argocd-readonly" {
   realm_id = keycloak_realm.sikademo.id
   name     = "argocd-readonly"
 }
+
+resource "keycloak_group" "argocd-proj-hello-world" {
+  realm_id = keycloak_realm.sikademo.id
+  name     = "argocd-proj-hello-world"
+}
+
+resource "keycloak_group" "argocd-foo" {
+  realm_id = keycloak_realm.sikademo.id
+  name     = "argocd-foo"
+}
