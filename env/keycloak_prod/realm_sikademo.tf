d872a6b555dd5df798e2416ad5896e1ac9e1c791
Ondrej Sika <ondrej@ondrejsika.com>
2023-12-15 09:55:14 +0100

    feat(env/keycloak_prod): Add realm sikademo

---
+++ env/keycloak_prod/realm_sikademo.tf
@@ -0,0 +1,40 @@
+resource "keycloak_realm" "sikademo" {
+  realm                    = "sikademo"
+  enabled                  = true
+  display_name_html        = "<h1>SikaDemo SSO</h1>"
+  login_with_email_allowed = true
+  reset_password_allowed   = true
+  remember_me              = true
+  login_theme              = "sikademo"
+}
+
+resource "keycloak_realm_events" "sikademo" {
+  lifecycle {
+    ignore_changes = [
+      events_listeners,
+    ]
+  }
+
+  realm_id = keycloak_realm.sikademo.id
+
+  events_enabled    = true
+  events_expiration = 3600
+
+  admin_events_enabled         = true
+  admin_events_details_enabled = true
+}
+
+resource "keycloak_openid_client_scope" "sikademo_groups" {
+  realm_id               = keycloak_realm.sikademo.id
+  name                   = "groups"
+  include_in_token_scope = true
+  gui_order              = 1
+}
+
+resource "keycloak_openid_group_membership_protocol_mapper" "sikademo_groups" {
+  realm_id        = keycloak_realm.sikademo.id
+  client_scope_id = keycloak_openid_client_scope.sikademo_groups.id
+  name            = "groups"
+  claim_name      = "groups"
+  full_path       = false
+}
